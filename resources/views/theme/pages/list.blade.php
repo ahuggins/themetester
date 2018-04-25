5e6a8664df2ffac0b3f23a5e6bf018170ec936fb
Andrew huggins <andrewhuggins@gmail.com>
2018-04-25 16:52:22 -0400

    adjustment for not having to set theme...coming from URL now

--- resources/views/theme/pages/list.blade.php
+++ resources/views/theme/pages/list.blade.php
@@ -1,7 +1,7 @@
 <h3>Pages</h3>
 @foreach ($files as $file)
     <div>
-        <a href="{{ url('themes/' . basename($file->getPath()) . '/' . str_replace('.blade.php', '', $file->getFilename())) }}">
+        <a href="{{ url('themes/' . $name . '/' .basename($file->getPath()) . '/' . str_replace('.blade.php', '', $file->getFilename())) }}">
             {{ $file->getFilename() }}
         </a>
     </div>
